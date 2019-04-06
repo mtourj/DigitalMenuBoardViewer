b8281d14bef550ab6a0a0ad17e4563a7cd1cd091
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-04-05 18:05:20 -0700

    Added ContentType enumuerator and integrated Firebase

---
+++ Assets/Scripts/ContentType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public enum ContentType
+{
+    Blank,
+    Tiles,
+    Image,
+    Title
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Firebase.Database;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    /// <summary>
+    /// This is the key used to retrieve a database reference from Firebase
+    /// </summary>
+    const string firebaseReferenceId = "";
+
+    /// <summary>
+    /// Insures single instance
+    /// </summary>
+    public static MenuManager instance;
+
+
+    static DatabaseReference dbRef;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Make sure only one instance of this exists
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Debug.LogWarning("Only one instance of MenuManager is allowed! Destroying additional instance.");
+            Destroy(this);
+        }
+
+        dbRef = FirebaseDatabase.DefaultInstance.GetReference(firebaseReferenceId);
         
     }
 
