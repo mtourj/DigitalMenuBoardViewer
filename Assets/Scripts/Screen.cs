dd2ad09a2a1fa055f2514903c6c8706f946f380a
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-04-13 01:53:36 -0700

    Added Screen prefab with attached Screen class
    The screen prefab allows us to create multiple instances of a screen for multiple displays

---
+++ Assets/Scripts/Screen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen : MonoBehaviour
+{
+
+    /// <summary>
+    /// A number between 1-8.
+    /// Each screen object corresponds to one
+    /// display, with a maximum of 8
+    /// </summary>
+    private int targetDisplay;
+
+    /// <summary>
+    /// What type of content is on this screen?
+    /// </summary>
+    private ContentType contentType;
+
+    /// <summary>
+    /// In order to initialize the screen object,
+    /// Setup() must be called.
+    /// </summary>
+    public void Setup(int targetDisplay, ContentType contentType)
+    {
+        this.targetDisplay = targetDisplay;
+        this.contentType = contentType;
+    }
+}
